4f0106c21d4e2a4feab987e8243c4a5d74eed9bc
MiyamizuSu <805108808t@gmail.com>
2024-06-16 15:22:33 +0800

    环境搭建

---
+++ MineSoftwareServer/src/main/java/com/MineSoftware/MineSoftwareServer/MineSoftwareServerApplication.java
@@ -0,0 +1,13 @@
+package com.MineSoftware.MineSoftwareServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MineSoftwareServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MineSoftwareServerApplication.class, args);
+    }
+
+}
---
+++ MineSoftwareServer/src/test/java/com/MineSoftware/MineSoftwareServer/MineSoftwareServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MineSoftware.MineSoftwareServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MineSoftwareServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
